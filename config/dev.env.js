8cc8693bca1a91a5f4de5c9e9b629254d290338b
DarrickAZ <zhwwhx@126.com>
2019-11-09 17:40:57 +0800

    ### modify js.

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,5 +5,5 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   //访问后台admin的地址
-  BASE_API: '"http://localhost:10001"'
+  BASE_API: '"http://localhost:10010"'
 })
